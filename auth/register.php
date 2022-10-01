2e44a6e845a24f5583d27d49b03da62a588c19d0
Mark Adrian De Jesus <67358671+iamarkadrian@users.noreply.github.com>
2022-10-01 09:07:12 +0800

    Added Register Form

---
+++ auth/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="./styles/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <input id="auth-register-email" type="email" placeholder="email">
+    <input id="auth-register-username" type="text" placeholder="Username">
+    <input id="auth-register-password" type="password" placeholder="Password">
+    <input id="auth-register-confirmPassword" type="password" placeholder="Confirm Password">
+    <button id="auth-btn-register">Register</button>
+</body>
+</html>
